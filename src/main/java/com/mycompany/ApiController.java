0762e057426cf00268ca871750ba752f1e95da11
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2026-01-17 23:36:53 +0200

    add new class, Response

--- src/main/java/com/mycompany/ApiController.java
+++ src/main/java/com/mycompany/ApiController.java
@@ -1,9 +1,14 @@
 package com.mycompany;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
 
+    @GetMapping("/api/v1/hello")
+    public String hello() {
+        return "Hello, World! Welcome to my API";
+    }
 }
---
+++ src/main/java/com/mycompany/Response.java
@@ -0,0 +1,29 @@
+package com.mycompany;
+
+public class Response {
+
+    private String message;
+    private String time;
+
+
+    public Response(String message) {
+        super();
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
